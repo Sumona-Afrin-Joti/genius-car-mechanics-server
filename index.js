800496a1629827ca458b01c93b6ef12fdbbab8e8
Sumona-Afrin-Joti <sumonajoti.06@gmail.com>
2021-10-29 09:29:17 +0600

    added port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
 
